bde9642b73833f02b1421c0f3f7487c0d221b2c5
Rens <r.walstijn@live.nl>
2016-11-22 10:28:59 +0100

    updated inputBehaviour

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -3,14 +3,11 @@ using System.Collections;
 
 public class PlayerInput : InputBehaviour {
     void FixedUpdate() {
-        if(base.EscapeKey) {
-            Debug.Log("pauze");
-        }
-        if(base.Horizontal != 999) {
-            Debug.Log("Horizontal: " + base.Horizontal);
-        }
-        if(base.Vertical != 999){
-            Debug.Log("Vertical: " + base.Vertical);
-        }
+        if(base.Escape){ Debug.Log("pauze");}
+        if(base.Space){  Debug.Log("space");}
+        if(base.W){ Debug.Log("W");}
+        if(base.A){ Debug.Log("A");}
+        if(base.S){ Debug.Log("S");}
+        if(base.D){ Debug.Log("D");}
     }
 }
