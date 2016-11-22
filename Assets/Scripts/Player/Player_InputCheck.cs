6bb4bc529105f97d2b1b40c3647d4f8e668d9ad9
DBeumans <buncho.gamingnetwork@gmail.com>
2016-11-22 11:33:44 +0100

    Deleted playerInput script , removed code to player_inputcheck script because they do the same thing.

--- Assets/Scripts/Player/Player_InputCheck.cs
+++ Assets/Scripts/Player/Player_InputCheck.cs
@@ -20,5 +20,18 @@ public class Player_InputCheck : InputBehaviour {
         if(ShootKey)
         { _player_Shooting.CreateBullet(); }
 
+        if (base.EscapeKey)
+        {
+            Debug.Log("pauze");
+        }
+        if (base.Horizontal != 999)
+        {
+            Debug.Log("Horizontal: " + base.Horizontal);
+        }
+        if (base.Vertical != 999)
+        {
+            Debug.Log("Vertical: " + base.Vertical);
+        }
+
     }
 }
