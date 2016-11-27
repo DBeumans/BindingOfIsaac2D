a7da5b1164e61755d579f81880668c5f3e095aaa
DBeumans <buncho.gamingnetwork@gmail.com>
2016-11-27 14:37:39 +0100

    Added spawner script, changed some lines in speedbehaviour and player_movement, comment lines in speedbehaviour , uncomment player shooting audio

--- Assets/Scripts/GameBehaviour/SpeedBehaviour.cs
+++ Assets/Scripts/GameBehaviour/SpeedBehaviour.cs
@@ -9,7 +9,7 @@ public class SpeedBehaviour : MonoBehaviour {
     //uncomment if Rigidbody3D is in needed objeckt
     //private Rigidbody3D _rgb3D;
     //private Vector3 _rgbVelocity3D;
-
+    /*
     private float _maxSpeed;
     private float _addSpeed;
 	private float _speed;
@@ -42,21 +42,26 @@ public class SpeedBehaviour : MonoBehaviour {
     }
 
     public bool speedUp { set { _speedUp = value;} }
-
-    public void startCoroutine() {
+    */
+    public void Coroutine() {
         StartCoroutine(SlowMovementRB2D());
         //StartCoroutine(SlowMovementRB3D());
     }
     
+    void Start()
+    {
+        _rgb2D = GetComponent<Rigidbody2D>();
+    }
 
     //comment if Rigidbody2D is in unneeded objeckt
     public Vector2 rigidbodyVelocity2D { get { return _rgbVelocity2D;} }
-    public Rigidbody2D rigidbody2D { set { _rgb2D = value;} }
+    public Rigidbody2D rigidbody2D {
+        get { return _rgb2D; } set { _rgb2D = value;} }
 
     //uncomment if Rigidbody3D is in needed objeckt
     /*public Vector3 rigidbodyVelocity3D { get { return _rgbVelocity3D;} }
     public Rigidbody3D rigidbody3D { set { _rgb3D = value;} }*/
-
+    /*
     public IEnumerator SlowMovementSB() {
         while(true) 
         {
@@ -68,12 +73,12 @@ public class SpeedBehaviour : MonoBehaviour {
             yield return new WaitForSeconds(.025f);
         }
     }
-
+    */
     //comment if Rigidbody2D is in unneeded objeckt
-    public IEnumerator SlowMovementRB2D() {
+    IEnumerator SlowMovementRB2D() {
         while(true)
         {
-            _rgbVelocity2D = _rgb2D.velocity * 0.9f;
+            _rgb2D.velocity = _rgb2D.velocity * 0.9f;
             yield return new WaitForSeconds(.025f);
         }
     }
--- Assets/Scripts/Player/Player_Movement.cs
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -14,7 +14,7 @@ public class Player_Movement : MonoBehaviour {
         _slowMovement = GetComponent<SpeedBehaviour>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _keyInput = GetComponent<InputBehaviour>();
-        _slowMovement.startCoroutine();
+        _slowMovement.Coroutine();
     }
 
     void FixedUpdate() {
@@ -22,6 +22,6 @@ public class Player_Movement : MonoBehaviour {
         _moveInput = _keyInput.MoveInput;
         _rigidbody2D.AddForce(_moveInput * _movementSpeed);
         _slowMovement.rigidbody2D = _rigidbody2D;
-        _rigidbody2D.velocity = _slowMovement.rigidbodyVelocity2D;
+        //_rigidbody2D.velocity = _slowMovement.rigidbody2D.velocity;
     }
 }
