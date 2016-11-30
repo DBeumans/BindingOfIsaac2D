1b6dd17e1ac5e3f69b3a19d3112e818b758589a9
DBeumans <buncho.gamingnetwork@gmail.com>
2016-11-30 14:04:39 +0100

    changed the enemy_movement script

--- Assets/Scripts/Enemy/Enemy_Movement.cs
+++ Assets/Scripts/Enemy/Enemy_Movement.cs
@@ -2,30 +2,30 @@
 using System.Collections;
 
 public class Enemy_Movement : MonoBehaviour {
-    RandomInputBehaviour _randInput;
-    SpeedBehaviour _slowMovement;
-    Rigidbody2D _rigidbody2D;
 
-    float _movementSwitchTime = 2;
-    float _movementSpeed = 2f;
-    Vector2 _moveInput;
-
-    void Start() {
-        //_randInput = GetComponent<RandomInputBehaviour();
-        _slowMovement = GetComponent<SpeedBehaviour>();
-        _rigidbody2D = GetComponent<Rigidbody2D>();
-        _slowMovement.Coroutine();
+    float _movementSpeed = .5f;
+    [SerializeField]Vector2 _moveInput;
+    
+    void Start()
+    {
+        StartCoroutine(RandomVector2());
     }
 
-    void FixedUpdate() {
-        _movementSwitchTime -= Time.deltaTime;
-        if(_movementSwitchTime <= 0) {   
-            _randInput.RandomF();
-            _movementSwitchTime = 2;
-        } 
-        
-        _moveInput = _randInput.MoveInput;
-        _rigidbody2D.AddForce(_moveInput * _movementSpeed);
-        _slowMovement.rigidbody2D = _rigidbody2D;
+    void FixedUpdate() { Movement(); }
+
+    void Movement()
+    {
+        transform.Translate((_moveInput * _movementSpeed)* Time.deltaTime);
+    }
+
+    IEnumerator RandomVector2()
+    {
+        while(true)
+        {
+            float x = Random.Range(-1f, 1f);
+            float y = Random.Range(-1f, 1f);
+            _moveInput = new Vector2(x, y);
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
