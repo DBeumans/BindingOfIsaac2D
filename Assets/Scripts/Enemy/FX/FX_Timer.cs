72bf02ac8f0148858c7ea7f40a6aab9f0ffc988d
DBeumans <buncho.gamingnetwork@gmail.com>
2016-12-08 23:54:38 +0100

    Fixed collisions, added new scene 'Game_Scene' where the new sprites are init, added enemy blood fx, when the enemy dies you see blood on the ground, added also a fx_timer where the blood slowly fades out and destroys when its alpha value is 0.

---
+++ Assets/Scripts/Enemy/FX/FX_Timer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FX_Timer : MonoBehaviour {
+
+    int _duration = 10;
+    float _opacity = 1f;
+    SpriteRenderer _spriteRenderer;
+
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        StartCoroutine(Opacity());
+    }
+
+    IEnumerator Opacity()
+    {
+        while (_duration > 0)
+        {
+            if(_spriteRenderer.color.a >0)
+            {
+                _duration -= 1 ;
+                Debug.Log(_duration);
+                _opacity -= .1f;
+                _spriteRenderer.color = new Color(255, 255, 255, _opacity);
+                // lower object opacity while its visible
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
+        Debug.Log("Puddle destroyed");
+        // when opacity ( alpha ) is 0 destroy the object.
+        Destroy(this.gameObject);
+    }
+}
